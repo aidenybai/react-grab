9134d2a76b6e3a0990998eba4f729168d36a369e
Aiden Bai <aiden.bai05@gmail.com>
2025-12-23 15:59:32 -0800

    fix

--- packages/react-grab/src/components/renderer.tsx
+++ packages/react-grab/src/components/renderer.tsx
@@ -83,49 +83,51 @@ export const ReactGrabRenderer: Component<ReactGrabRendererProps> = (props) => {
                 />
               )}
             </For>
-            <SelectionLabel
-              tagName={session().tagName}
-              componentName={session().componentName}
-              selectionBounds={session().selectionBounds[0]}
-              mouseX={session().position.x}
-              visible={true}
-              hasAgent={true}
-              isAgentConnected={true}
-              status={session().isStreaming ? "copying" : "copied"}
-              statusText={session().lastStatus || "Thinking…"}
-              inputValue={session().context.prompt}
-              previousPrompt={session().context.prompt}
-              supportsUndo={props.supportsUndo}
-              supportsFollowUp={props.supportsFollowUp}
-              dismissButtonText={props.dismissButtonText}
-              onAbort={() => props.onAbortSession?.(session().id)}
-              onDismiss={
-                session().isStreaming
-                  ? undefined
-                  : () => props.onDismissSession?.(session().id)
-              }
-              onUndo={
-                session().isStreaming
-                  ? undefined
-                  : () => props.onUndoSession?.(session().id)
-              }
-              onFollowUpSubmit={
-                session().isStreaming
-                  ? undefined
-                  : (prompt) =>
-                      props.onFollowUpSubmitSession?.(session().id, prompt)
-              }
-              error={session().error}
-              onAcknowledgeError={() =>
-                props.onAcknowledgeSessionError?.(session().id)
-              }
-              onRetry={() => props.onRetrySession?.(session().id)}
-              isPendingAbort={
-                session().isStreaming ? props.isPendingAgentAbort : false
-              }
-              onConfirmAbort={props.onConfirmAgentAbort}
-              onCancelAbort={props.onCancelAgentAbort}
-            />
+            <Show when={session().selectionBounds.length > 0}>
+              <SelectionLabel
+                tagName={session().tagName}
+                componentName={session().componentName}
+                selectionBounds={session().selectionBounds[0]}
+                mouseX={session().position.x}
+                visible={true}
+                hasAgent={true}
+                isAgentConnected={true}
+                status={session().isStreaming ? "copying" : "copied"}
+                statusText={session().lastStatus || "Thinking…"}
+                inputValue={session().context.prompt}
+                previousPrompt={session().context.prompt}
+                supportsUndo={props.supportsUndo}
+                supportsFollowUp={props.supportsFollowUp}
+                dismissButtonText={props.dismissButtonText}
+                onAbort={() => props.onAbortSession?.(session().id)}
+                onDismiss={
+                  session().isStreaming
+                    ? undefined
+                    : () => props.onDismissSession?.(session().id)
+                }
+                onUndo={
+                  session().isStreaming
+                    ? undefined
+                    : () => props.onUndoSession?.(session().id)
+                }
+                onFollowUpSubmit={
+                  session().isStreaming
+                    ? undefined
+                    : (prompt) =>
+                        props.onFollowUpSubmitSession?.(session().id, prompt)
+                }
+                error={session().error}
+                onAcknowledgeError={() =>
+                  props.onAcknowledgeSessionError?.(session().id)
+                }
+                onRetry={() => props.onRetrySession?.(session().id)}
+                isPendingAbort={
+                  session().isStreaming ? props.isPendingAgentAbort : false
+                }
+                onConfirmAbort={props.onConfirmAgentAbort}
+                onCancelAbort={props.onCancelAgentAbort}
+              />
+            </Show>
           </>
         )}
       </Index>
