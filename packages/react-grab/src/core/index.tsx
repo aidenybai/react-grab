171b621d7f896258d1710b0ba3c9b200faef461d
Aiden Bai <aiden.bai05@gmail.com>
2026-01-05 00:08:03 -0800

    fix

--- packages/react-grab/src/core/index.tsx
+++ packages/react-grab/src/core/index.tsx
@@ -2418,7 +2418,7 @@ export const init = (rawOptions?: Options): ReactGrabAPI => {
         isPromptMode: isPromptMode(),
         targetElement: targetElement(),
         dragBounds: dragBounds() ?? null,
-        grabbedBoxes: computedGrabbedBoxes().map((box) => ({
+        grabbedBoxes: store.grabbedBoxes.map((box) => ({
           id: box.id,
           bounds: box.bounds,
           createdAt: box.createdAt,
