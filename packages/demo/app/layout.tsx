b96b6e67a802ff92f2408733326719d827635917
Aiden Bai <aiden.bai05@gmail.com>
2026-03-05 00:22:55 -0800

    demo

--- packages/demo/app/layout.tsx
+++ packages/demo/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   subsets: ["latin"],
@@ -28,6 +29,10 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Script
+          src="//unpkg.com/react-grab/dist/index.global.js"
+          strategy="beforeInteractive"
+        />
       </body>
     </html>
   );
