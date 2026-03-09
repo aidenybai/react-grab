a04139a12467a3df9fbbaa0671763b0908ceca71
Aiden Bai <aiden.bai05@gmail.com>
2026-03-09 03:42:51 -0700

    Enhance automation support in installation process by introducing REACT_GRAB_INIT environment variable and providing setup instructions for automated environments.

--- packages/react-grab/scripts/postinstall.cjs
+++ packages/react-grab/scripts/postinstall.cjs
@@ -11,11 +11,13 @@ const AUTOMATION_ENVIRONMENT_VARIABLE_NAMES = [
 const REACT_GRAB_INIT_COMMAND = "npx -y grab@latest init";
 const INSTALL_HINT_MESSAGE = `[react-grab] Package installed via automation. This step only installs the package. Run \`${REACT_GRAB_INIT_COMMAND}\` to complete setup.`;
 
+const isAlreadyRunningInit = Boolean(process.env.REACT_GRAB_INIT);
+
 const isRunningInAutomatedEnvironment =
   AUTOMATION_ENVIRONMENT_VARIABLE_NAMES.some((environmentVariableName) =>
     Boolean(process.env[environmentVariableName]),
   );
 
-if (isRunningInAutomatedEnvironment) {
+if (isRunningInAutomatedEnvironment && !isAlreadyRunningInit) {
   console.log(INSTALL_HINT_MESSAGE);
 }
