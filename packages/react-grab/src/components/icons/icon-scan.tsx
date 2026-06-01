cf7c8535330cfca6f7e2071bf93a51556a5ca90a
Aiden Bai <aiden.bai05@gmail.com>
2026-05-31 23:49:19 -0700

    feat(react-grab): use a filled alert-diamond icon for scan

--- packages/react-grab/src/components/icons/icon-scan.tsx
+++ packages/react-grab/src/components/icons/icon-scan.tsx
@@ -14,17 +14,14 @@ export const IconScan: Component<IconScanProps> = (props) => {
       width={size()}
       height={size()}
       viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      fill="currentColor"
       class={props.class}
     >
-      <path d="M3 7V5a2 2 0 0 1 2-2h2" />
-      <path d="M17 3h2a2 2 0 0 1 2 2v2" />
-      <path d="M21 17v2a2 2 0 0 1-2 2h-2" />
-      <path d="M7 21H5a2 2 0 0 1-2-2v-2" />
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M2.7 10.3a2.41 2.41 0 0 0 0 3.41l7.59 7.59a2.41 2.41 0 0 0 3.41 0l7.59-7.59a2.41 2.41 0 0 0 0-3.41L13.7 2.71a2.41 2.41 0 0 0-3.41 0ZM12 7.5a1 1 0 0 1 1 1v3a1 1 0 0 1-2 0v-3a1 1 0 0 1 1-1ZM13.05 15.5a1.05 1.05 0 1 0-2.1 0 1.05 1.05 0 0 0 2.1 0Z"
+      />
     </svg>
   );
 };
