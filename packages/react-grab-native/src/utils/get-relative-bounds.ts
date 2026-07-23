045271bcdd1966c600fd2e33ec8195fd5cef4812
Aiden Bai <aiden.bai05@gmail.com>
2026-07-23 10:34:46 +0000

    avoid untrusted native highlight coordinates

--- packages/react-grab-native/src/utils/get-relative-bounds.ts
+++ packages/react-grab-native/src/utils/get-relative-bounds.ts
@@ -3,8 +3,11 @@ import type { HostBounds } from "react-grab/targets";
 export const getRelativeBounds = (
   bounds: HostBounds,
   parentBounds: HostBounds | null,
-): HostBounds => ({
-  ...bounds,
-  x: bounds.x - (parentBounds?.x ?? 0),
-  y: bounds.y - (parentBounds?.y ?? 0),
-});
+): HostBounds | null =>
+  parentBounds
+    ? {
+        ...bounds,
+        x: bounds.x - parentBounds.x,
+        y: bounds.y - parentBounds.y,
+      }
+    : null;
